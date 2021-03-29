64d3ef5741b7585224fc86ddf146945a907e2e36
Jefferzom <jeffsilvati@hotmail.com>
2021-03-29 00:27:01 -0300

    resolve issues preview

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,8 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from "next"
+import { useSession } from "next-auth/client"
 
 import Head from 'next/head'
 import Link from "next/link"
+import { useRouter } from "next/router"
 import { RichText } from "prismic-dom"
+import { useEffect } from "react"
 import { getPrismicClient } from "../../../services/prismic"
 
 import styles from '../post.module.scss'
@@ -17,6 +20,15 @@ interface PostPreviewProps {
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`)
+    }
+  }, [session])
+
   return (
     <>
       <Head>
@@ -35,7 +47,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
           <div className={styles.continueReading}>
             Wanna continue reading??
             <Link href="/">
-              <a href="">Subscribe now 🤗</a>
+              <a href=""> Subscribe now 🤗</a>
             </Link>
           </div>
         </article>
